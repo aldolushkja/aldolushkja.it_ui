fa6b21e54156ba24323fe68981f1c4bbf28acb16
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-12-14 01:12:00 +0100

    refactoring

--- scripts/3_changeFilePermissions.sh
+++ scripts/3_changeFilePermissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 echo "Cambia permessi ai file"
-sudo su - && \
+ sudo -S /path/to/command sudo su - && \
 sudo chown -RH root: /var/www/aldolushja.it/ && \
 sudo find /var/www/alushkja.it -type f -exec sudo chmod 644 {} \; && \
 sudo find /var/www/alushkja.it -type d -exec chmod 755 {} \;
